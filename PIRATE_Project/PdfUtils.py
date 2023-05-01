ad302899fedebf216c2edb782edf77f8d2c90200
moorl007 <93689704+moorl007@users.noreply.github.com>
2023-05-01 13:46:19 -0500

    Add PdfUtils.py

---
+++ PIRATE_Project/PdfUtils.py
@@ -0,0 +1,59 @@
+import requests
+import fitz
+
+#get the url to the pdf
+#will return None if pdf can't be obtained
+def get_pdf_url(paper_id) -> str:
+    url = 'https://api.semanticscholar.org/graph/v1/paper/' + paper_id + '?fields=openAccessPdf,title'
+    response = requests.get(url)
+
+    if response.status_code == 200:
+        # content = json.loads(response.content.decode("utf-8"))["data"]
+        response_json = response.json()
+        print(response_json)
+        if "openAccessPdf" in response_json:
+            if response_json["openAccessPdf"] != None:
+                raw_text_url = response_json["openAccessPdf"]["url"]
+                print(raw_text_url)
+                return raw_text_url
+            else:
+                return None
+        else:
+            print('no open access')
+            return None 
+    else:
+        print('response is none')
+        return None
+
+def get_pdf_text_sections(link) -> list:
+    retVal = []
+    pdf_response = requests.get(link)#this may take awhile if bad link
+    if pdf_response.status_code == 200:
+        print('ok link')
+        # Save the content of the response to a file
+        with open("sample.pdf", "wb") as f:
+            f.write(pdf_response.content)
+    else:
+        print('bad link')
+        return None
+    
+    doc = fitz.open('sample.pdf')
+    for page in doc:
+        text = page.get_text("blocks")  # get plain text
+        # print(text[0])
+        for elem in text:
+            to_add = elem[4]
+            to_add = to_add.replace('-\n', '')
+            to_add = to_add.replace('\n', ' ')
+            retVal.append(to_add)
+    return retVal
+
+def get_text(paper_id) -> list:
+        '''FOR USE
+        Returns a list of the sections in the pdf document
+        Requires the paper ID string'''
+        link = get_pdf_url(paper_id)
+        if(link == None):
+            return None
+        retVal = get_pdf_text_sections(link)
+        return retVal
